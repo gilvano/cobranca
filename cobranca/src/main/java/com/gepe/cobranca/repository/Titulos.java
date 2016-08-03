a64269fb217504b4b1cd206ca74c3ea1c8a8e5c7
gilpi <gilpi@DESKTOP-4PNTU7F>
2016-08-02 21:30:15 -0300

    commit 2

--- cobranca/src/main/java/com/gepe/cobranca/repository/Titulos.java
+++
@@ -1,9 +0,0 @@
-package com.gepe.cobranca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gepe.cobranca.model.Titulo;
-
-public interface Titulos extends JpaRepository<Titulo, Long>{
-
-}
